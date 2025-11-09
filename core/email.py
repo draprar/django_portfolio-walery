5733647689743b952a7c6bfeaf15eef2d8382d42
Walery <waleron@gmail.com>
2025-11-09 12:54:48 +0100

    secure contact

--- core/email.py
+++ core/email.py
@@ -1,12 +1,26 @@
+import logging
 import sib_api_v3_sdk
 from sib_api_v3_sdk.rest import ApiException
 from django.conf import settings
+import bleach
+
+logger = logging.getLogger(__name__)
 
 def send_brevo_email(subject, html_content, recipient_list):
     configuration = sib_api_v3_sdk.Configuration()
     configuration.api_key['api-key'] = settings.BREVO_API_KEY
 
-    api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+    api_client = sib_api_v3_sdk.ApiClient(configuration)
+    api_client.rest_client.pool_manager.connection_pool_kw['timeout'] = 10
+    api_instance = sib_api_v3_sdk.TransactionalEmailsApi(api_client)
+
+    # Sanity check / sanitization
+    subject = bleach.clean(subject, strip=True)
+    html_content = bleach.clean(
+        html_content,
+        tags=['p', 'b', 'br', 'i', 'strong', 'a'],
+        attributes={'a': ['href']}
+    )
 
     sender = {"name": "Moja Aplikacja", "email": settings.DEFAULT_FROM_EMAIL}
     to = [{"email": email} for email in recipient_list]
@@ -21,6 +35,6 @@ def send_brevo_email(subject, html_content, recipient_list):
     try:
         response = api_instance.send_transac_email(send_smtp_email)
         return response
-    except (ApiException, Exception) as e:
-        print("Błąd podczas wysyłania maila z Brevo: %s\n" % e)
+    except (ApiException, Exception):
+        logger.exception("Błąd podczas wysyłania maila z Brevo")
         return None
